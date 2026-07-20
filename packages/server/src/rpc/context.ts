144d7492fb2f3496aaa764e51009a3151fd9a2f2
oxwen11 <iamoxwen@gmail.com>
2026-07-20 01:19:46 -0400

    fix(rpc): drop unused HarnessAgentSessionService import

--- packages/server/src/rpc/context.ts
+++ packages/server/src/rpc/context.ts
@@ -3,7 +3,6 @@ import type { FileSystem } from "effect/FileSystem";
 
 import type { EventBus } from "../events";
 import type { FileSystemService } from "../fs";
-import type { HarnessAgentSessionService } from "../harness";
 import type { ProjectService } from "../project";
 import type { SessionService } from "../session";
 
